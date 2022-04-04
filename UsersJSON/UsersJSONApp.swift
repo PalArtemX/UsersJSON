6f9e57dd2b2718b96b5e67de71ed9c2eef857ee4
Artem Paliutin <pal.artem@icloud.com>
2022-04-04 20:40:10 +0300

    Initial Commit

---
+++ UsersJSON/UsersJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersJSONApp.swift
+//  UsersJSON
+//
+//  Created by Artem Paliutin on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct UsersJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
