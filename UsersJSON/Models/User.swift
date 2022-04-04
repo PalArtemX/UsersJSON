1bad2dd30825bd4274ce0352455e1cda7b428ea4
Artem Paliutin <pal.artem@icloud.com>
2022-04-04 21:27:04 +0300

    start

---
+++ UsersJSON/Models/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  UsersJSON
+//
+//  Created by Artem Paliutin on 04/04/2022.
+//
+
+import Foundation
+
+
+// MARK: - User
+struct User: Codable {
+    let id: Int
+    let name, username, email: String
+    let address: Address
+    let phone, website: String
+    let company: Company
+    
+    
+    // MARK: - Address
+    struct Address: Codable {
+        let street, suite, city, zipcode: String
+        let geo: Geo
+    }
+    // MARK: - Geo
+    struct Geo: Codable {
+        let lat, lng: String
+    }
+    // MARK: - Company
+    struct Company: Codable {
+        let name, catchPhrase, bs: String
+    }
+}
+
+
---
+++ UsersJSON/Preview Content/previewUser.swift
@@ -0,0 +1,10 @@
+//
+//  previewUser.swift
+//  UsersJSON
+//
+//  Created by Artem Paliutin on 04/04/2022.
+//
+
+import Foundation
+
+
