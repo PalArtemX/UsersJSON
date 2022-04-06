757db6c4f606de9ac9432e3e5a0c69df978fea91
Artem Paliutin <pal.artem@icloud.com>
2022-04-06 19:12:34 +0300

    fix

--- UsersJSON/Utilities/NetworkingManager.swift
+++ UsersJSON/Utilities/NetworkingManager.swift
@@ -9,20 +9,37 @@ import Foundation
 import Combine
 
 class NetworkingManager {
+    
+    // MARK: - NetworkingError
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url):
+               return "[⚠️] Bad response from URL: \(url)"
+            case .unknown:
+               return "[⚠️] Unknown error"
+            }
+        }
+        
+    }
+    
     // MARK: - download
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         URLSession.shared.dataTaskPublisher(for: url)
-            .tryMap({ try handleURLResponse(output: $0 )})
+            .tryMap({ try handleURLResponse(output: $0, url: url )})
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
     // MARK: - handleURLResponse
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
         guard
             let response = output.response as? HTTPURLResponse,
             response.statusCode >= 200 && response.statusCode < 300 else {
-            throw URLError(.badServerResponse)
+            throw NetworkingError.badURLResponse(url: url)
         }
         return output.data
     }
